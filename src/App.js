03832e5db22ea3e799395e2e6d24d3daae18eca5
tuyen-cpu-i5 <18130277@st.hcmuaf.edu.vn>
2021-09-23 00:08:32 +0700

    complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import TodoList from "./component/TodoList";
 import Textfield from "@atlaskit/textfield";
 import Button from "@atlaskit/button";
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { v4 } from "uuid";
+
+const TODO_APP_STORAGE_KEY = "TODO_APP";
+
 function App() {
   const [todoList, setTodoList] = useState([]);
   const [textInput, setTextInput] = useState("");
+  useEffect(() => {
+    const storagedTodoList = localStorage.getItem(TODO_APP_STORAGE_KEY);
+    if (storagedTodoList) {
+      setTodoList(JSON.parse(storagedTodoList));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(TODO_APP_STORAGE_KEY, JSON.stringify(todoList));
+  }, [todoList]);
+
   const onTextInputChange = useCallback((e) => {
     setTextInput(e.target.value);
   }, []);
   const onAddBtnClick = useCallback(
     (e) => {
       setTodoList([
-        
-        { id: v4(), name: textInput, isCompleted: false },...todoList,
+        { id: v4(), name: textInput, isCompleted: false },
+        ...todoList,
       ]);
-      setTextInput("")
+      setTextInput("");
     },
     [textInput, todoList]
   );
+  const onCheckBtnClick = useCallback((id) => {
+    setTodoList((prevState) =>
+      prevState.map((todo) =>
+        todo.id === id ? { ...todo, isCompleted: true } : todo
+      )
+    );
+  }, []);
   return (
     <>
       <h3> Danh sách cần làm </h3>
@@ -38,7 +59,7 @@ function App() {
         value={textInput}
         onChange={onTextInputChange}
       ></Textfield>
-      <TodoList todoList={todoList} />
+      <TodoList todoList={todoList} onCheckBtnClick={onCheckBtnClick} />
     </>
   );
 }
